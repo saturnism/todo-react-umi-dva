85c0a25984800345f2f9169550cefd2ac69c18dd
Ray Tsang <saturnism@gmail.com>
2019-03-31 16:01:56 -0400

    drag and drop list

--- src/pages/planner/index.tsx
+++ src/pages/planner/index.tsx
@@ -9,19 +9,47 @@ import { DragDropContextProvider } from 'react-dnd'
 import HTML5Backend from 'react-dnd-html5-backend'
 import DragTag from '@/components/DragTag';
 import DropCell from '@/components/DropCell';
-
+import { arrayMove, SortableContainer, SortableElement, SortEvent } from 'react-sortable-hoc';
 
 class ViewProps {
 }
 
 class ViewStates {
+  items: string[]
 }
 
+const SortableItem = SortableElement(({value}) => <Timeline.Item>{value}</Timeline.Item>)
+
+const SortableList = SortableContainer(({items}) => {
+  return (
+    <Timeline>
+      {items.map((value, index) => (
+        <SortableItem key={`item-${index}`} index={index} value={value} />
+      ))}
+    </Timeline>
+  );
+});
+
+
 class Counter extends PureComponent<ViewProps, ViewStates> {
-  state = new ViewStates();
+  state = {
+    items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6']
+  };
+
+
+  onSortEnd = ({oldIndex, newIndex}, e : SortEvent) => {
+    this.setState(({items}) => ({
+      items: arrayMove(items, oldIndex, newIndex),
+    }));
+  };
 
   render() {
     return (
+      <>
+        <div style={{ width: 100 }}>
+          <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />
+        </div>
+
       <DragDropContextProvider backend={HTML5Backend}>
         <Row>
           <DragTag>One</DragTag>
@@ -31,6 +59,7 @@ class Counter extends PureComponent<ViewProps, ViewStates> {
           <Calendar dateCellRender={this.renderDateCell} ></Calendar>
         </Row>
       </DragDropContextProvider>
+        </>
     );
   }
 
